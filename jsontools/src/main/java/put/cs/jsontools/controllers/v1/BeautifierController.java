322b20aede7ccc573f111d6de09230faa083daf1
AdamJurcz <adamos00@op.pl>
2022-12-05 17:49:02 +0100

    Added beautifier service interface. Closes #4.

---
+++ jsontools/src/main/java/put/cs/jsontools/controllers/v1/BeautifierController.java
@@ -0,0 +1,13 @@
+package put.cs.jsontools.controllers.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(BeautifierController.BASE_URL)
+public class BeautifierController {
+    public static final String BASE_URL = "/api/v1/beautifier";
+
+
+
+}
---
+++ jsontools/src/main/java/put/cs/jsontools/services/BeautifierService.java
@@ -0,0 +1,5 @@
+package put.cs.jsontools.services;
+
+public interface BeautifierService {
+    String getBeautifiedJson(String json);
+}
