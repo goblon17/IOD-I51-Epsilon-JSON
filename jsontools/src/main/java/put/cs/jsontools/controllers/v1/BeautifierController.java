e5f32081ebd28ed098a96de81b275a2b87cd2074
AdamJurcz <adamos00@op.pl>
2022-12-06 11:46:02 +0100

    Created beautifier and minifier controllers. Closes #2.

--- jsontools/src/main/java/put/cs/jsontools/controllers/v1/BeautifierController.java
+++ jsontools/src/main/java/put/cs/jsontools/controllers/v1/BeautifierController.java
@@ -1,13 +1,24 @@
 package put.cs.jsontools.controllers.v1;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import put.cs.jsontools.services.BeautifierService;
 
 @RestController
 @RequestMapping(BeautifierController.BASE_URL)
 public class BeautifierController {
     public static final String BASE_URL = "/api/v1/beautifier";
 
+    private final BeautifierService beautifierService;
 
+    public BeautifierController(BeautifierService beautifierService) {
+        this.beautifierService = beautifierService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String beautifyJson(@RequestBody String json){
+        return beautifierService.getBeautifiedJson(json);
+    }
 
 }
--- jsontools/src/main/java/put/cs/jsontools/controllers/v1/MinifierController.java
+++ jsontools/src/main/java/put/cs/jsontools/controllers/v1/MinifierController.java
@@ -1,13 +1,24 @@
 package put.cs.jsontools.controllers.v1;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import put.cs.jsontools.services.MinifierService;
 
 @RestController
 @RequestMapping(MinifierController.BASE_URL)
 public class MinifierController {
     public static final String BASE_URL = "/api/v1/minifier";
 
+    private final MinifierService minifierService;
 
+    public MinifierController(MinifierService minifierService) {
+        this.minifierService = minifierService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String minifyJson(@RequestBody String json){
+        return minifierService.getMinificatedJson(json);
+    }
 
 }
