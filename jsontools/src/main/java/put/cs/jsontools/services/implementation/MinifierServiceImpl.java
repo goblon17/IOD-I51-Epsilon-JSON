fdc4fa1eeddd75b4bbb8f051b815064d61419e0f
AdamJurcz <adamos00@op.pl>
2022-12-22 00:03:37 +0100

    Refactored Services.

--- jsontools/src/main/java/put/cs/jsontools/services/implementation/MinifierServiceImpl.java
+++ jsontools/src/main/java/put/cs/jsontools/services/implementation/MinifierServiceImpl.java
@@ -1,23 +1,24 @@
 package put.cs.jsontools.services.implementation;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import put.cs.jsontools.exceptions.FieldInJsonNotFoundException;
 import put.cs.jsontools.exceptions.InvalidJsonFormatException;
 import put.cs.jsontools.services.MinifierService;
-import put.cs.jsontools.transformations.MinifierTransformation;
+
+import put.cs.jsontools.transforms.JsonTransformer;
 
 @Service
 public class MinifierServiceImpl implements MinifierService {
 
-    private final MinifierTransformation transformation;
+    private final JsonTransformer jsonTransformer;
 
-    public MinifierServiceImpl(MinifierTransformation transformation) {
-        this.transformation = transformation;
+    public MinifierServiceImpl(@Qualifier("beautifierTransformerImpl") JsonTransformer jsonTransformer) {
+        this.jsonTransformer = jsonTransformer;
     }
 
     @Override
     public String getMinificatedJson(String json) throws InvalidJsonFormatException{
-        String result = transformation.transformFromBeautyToUgly(json);
+        String result = jsonTransformer.transform(json, null);
         if(result==null){
             throw new InvalidJsonFormatException();
         }
